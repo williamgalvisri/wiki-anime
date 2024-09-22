d31ab419c74e8ab300b0c915b9868a52accd994a
triplew.3598 <triplew.3598@gmail.com>
2024-09-22 19:44:11 +0000

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
